c9c974ee72200e6f56212763e1056aa9c805a638
Dev.Dalton <kagimudalton@gmail.com>
2026-09-11 15:20:04 +0300

    feat: mount KDev System Center in the desktop shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import SystemCenter from "./SystemCenter";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
+    <SystemCenter />
   </React.StrictMode>
 );
