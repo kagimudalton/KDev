0fc9eb3246af127781f1d4af6e84f7c241299fe4
Dev.Dalton <kagimudalton@gmail.com>
2026-09-11 15:57:32 +0300

    feat: wire final development control center

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import SystemCenter from "./SystemCenter";
 import KDevPowerPanel from "./KDevPowerPanel";
+import FinishCenter from "./FinishCenter";
 import "./styles.css";
 
 function Root() {
   const [powerOpen, setPowerOpen] = useState(false);
+  const [finishOpen, setFinishOpen] = useState(false);
   return <>
     <App />
     <SystemCenter />
     <button className="power-launch" onClick={() => setPowerOpen(true)} title="KDev Workspace Control">⌘</button>
+    <button className="finish-launch" onClick={() => setFinishOpen(true)} title="KDev Development Control Center">✦</button>
     {powerOpen && <KDevPowerPanel onClose={() => setPowerOpen(false)} />}
+    {finishOpen && <FinishCenter onClose={() => setFinishOpen(false)} />}
   </>;
 }
 
